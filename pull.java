7b031ddbcb63f90ab1a233a40b019b699c075439
Vinayak Shukla <youremail@example.com>
2024-10-08 12:24:50 +0530

    New main

--- pull.java
+++ pull.java
@@ -4,6 +4,6 @@ import java.util.*;
 
 public class pull{
     public static void main(String args[]){
-        System.out.println("Accept it bro, just practicing fork and pull");
+        System.out.println("Accept it bro, just practicing fork and pull2");
     }
 }
